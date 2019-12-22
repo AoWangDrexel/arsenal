f1ff5fd57d2b4ed1db5ecc7abfcc170df827c102
Ao Wang <aw3338@drexel.edu>
2019-12-22 12:40:49 -0500

    Add files via upload

--- crypto/detect_english.py
+++ crypto/detect_english.py
@@ -1,122 +1,82 @@
-"""
-This module implements English detection using two English dictionary text files.
+"""The English Dectection Module.
+
+An English detection implementation using two English dictionary text files.
 
 Attributes:
-    LETTERS_AND_SPACE: str
-        makeup of the alphabet and any white space
-    ENGLISH_WORDS: list
-        collection of the English words from the two text files
-
-Methods:
-    load_alphabet()
-        Returns a string of the lower and uppercase of the alphabet
-    load_dictionary()
-        Returns a list of English words
-    remove_non_letters(msg)
-        Returns message without any non-alphabetic symbols
-    get_english_count(msg)
-        Returns a percentage of the number of words in the English dictionary
-        out of total number of words in the message
-    find_max_ind(keys)
-        Returns the index of the greatest percentage decrypted message from the list
+    ENGLISH_WORDS: List
+        A list of the English words.
 """
-
-LETTERS_AND_SPACE = ""
-
-
-def load_alphabet():
-    """The function loops through the ASCII code and returns a string of the
-       lower and uppercase alphabet.
-    """
-    alphabet = ""
-    for i in range(26):
-        alphabet += chr(ord("A") + i)
-        alphabet += chr(ord("a") + i)
-    return alphabet
 
 
 def load_dictionary():
-    """The function returns a list of words from two word text files.
+    """Creates a dictionary of English words.
+    
+    Example
+    =======
+    >>> from cryptsenal.detect_english import *
+    >>> load_dictionary()
+    ['aarhus','aaron','ababa','aback','abaft',....]
+    
+    Args:
+        None
+    
+    Returns:
+        List (str): A list of English words.
     """
-    with open("words.txt", "r") as f1, \
-            open("morewords.txt", "r") as f2:
-        file1 = f1.read().split("\n")
-        file2 = f2.read().split("\n")
-
-    for i in range(len(file2)):
-        file2[i] = file2[i].lower()
-
+    with open("words.txt", "r") as f1, open("morewords.txt", "r") as f2:
+        file1, file2 = f1.read().split("\n"), f2.read().split("\n")
+    file1 = [word.lower() for word in file1]
+    file2 = [word.lower() for word in file2]
     file2.extend(file1)
     return file2
 
 
-LETTERS_AND_SPACE = load_alphabet() + " \t\n"
 ENGLISH_WORDS = load_dictionary()
 
 
 def remove_non_letters(msg):
-    """The function removes non characters in the LETTERS_AND_SPACE variable.
-
+    """Removes non-alphabetic symbols, except for whitespace.
+    
+    Example
+    =======
+    >>> from cryptsenal.detect_english import *
+    >>> remove_non_letters("How are? you? I am good....?")
+    How are you I am good
+    
     Args:
-        msg (str): English text
+        msg (str): A string of text.
 
     Returns:
-        str: English text without any other non-alphabetic symbols
-
-    Raises:
-        TypeError
-            If msg is not passed as an argument or msg is not a string type
+        str: A cleaned string of text.  
     """
-    lettersOnly = []
+    clean_words = []
     for symbol in msg:
-        if symbol in LETTERS_AND_SPACE:
-            lettersOnly.append(symbol)
-    return "".join(lettersOnly)
+        if symbol.isalpha() or symbol.isspace():
+            clean_words.append(symbol)
+    return "".join(clean_words)
 
 
 def get_english_count(msg):
     """The function returns the percentage of words in the dictionary.
-
+    
+    Example
+    =======
+    >>> from cryptsenal.detect_english import *
+    >>> get_english_count("How are you? I am good. Also wdffdsifnid Ha!")
+    88.88888888888889
+    
     Args:
-        msg (str): English text
-
+        msg (str): A string of text.
+        
     Returns:
-        double: Percentage of words in the English dictionary from total number of words in msg
-
-    Raises:
-        TypeError
-            If msg is not passed as an argument or msg is not a string type
+        float: A percentage of words in English. 
     """
-    msg = msg.lower()
-    msg = remove_non_letters(msg)
+    msg = remove_non_letters(msg.lower())
     possible_words = msg.split()
     if possible_words == []:
         return 0
     matches = 0
-
     for word in possible_words:
         if word in ENGLISH_WORDS:
             matches += 1
     return 100 * float(matches) / len(possible_words)
-
-
-def find_max_ind(keys):
-    """The function finds the highest percentage of words in the dictionary and returns the key
-
-        Args:
-            keys (list): list of percentages of the possibilities of English text
-
-        Returns:
-            int: the index of the highest percentage
-
-        Raises:
-            TypeError:
-                If keys is not passed as an argument or keys is not a list
-    """
-    maximum = -1
-    max_key = -1
-    for key in keys:
-        if keys[key] > maximum:
-            maximum = keys[key]
-            max_key = key
-    return max_key
--- crypto/morse.py
+++ crypto/morse.py
@@ -1,127 +1,103 @@
-"""
-This module implements the International Morse Code on text
-
-Example:
-    $ python morse.py
-
-    import morse
-    print(morse.encode("Hello World"))
-    > .... . .-.. .-.. --- | .-- --- .-. .-.. -..
+"""The Morse Code Module.
 
 Attributes:
     morse_dict: dict
         the dictionary version of the morseTable.txt
-
-Methods:
-    load_morse_table()
-        Returns a dictionary of the Morse Code
-    encode(plain_text)
-        Returns the encoded text
-    values_by_keys(value)
-        Returns the key by getting the value of the dictionary
-    decode(encoded_text)
-        Returns the decoded Morse Code text
 """
-
-morse_dict = {}
 
 
 def load_morse_table():
-    """The function retrieves the morse code from a text file and cleans it up so the letters
-       can be stored in the keys and code into the values
+    """Creates a dictionary of the Morse Code.
+    
+    Example
+    =======
+    >>> from cryptsenal.morse import *
+    >>> load_morse_table()
+    {',': '--..--','.': '.-.-.-','0': '-----',...,'Z': '--..'}
+    
+    Args:
+        None
+    
+    Returns:
+        dict (keys: str, values: str): A Morse Code dictionary. 
     """
     codes = ""
     with open("morseTable.txt", "r") as code:
         codes = code.read()
-
-    codes.split("\n")
     codes = codes.split()
-    keys = []
-    values = []
-
-    for i in range(len(codes)):
-        if(i % 2 == 0):
-            keys.append(codes[i])
-        else:
-            values.append(codes[i])
-
-    morse_dict = {}
-    for i in range(len(keys)):
-        morse_dict[keys[i]] = values[i]
+    keys = [codes[i] for i in range(0, len(codes), 2)]
+    values = [codes[i] for i in range(1, len(codes), 2)]
+    morse_dict = {keys[i]:values[i] for i in range(len(keys))}
     return morse_dict
 
 
+morse_dict = {}
 morse_dict = load_morse_table()
 
 
 def encode(plain_text):
-    """
-    The function encrypts the plaintext into ciphertext and returns the string.
-
+    """Encodes the message in Morse Code.
+    
+    Example
+    =======
+    >>> from cryptsenal.morse import *
+    >>> encode("How are you today?")
+    .... --- .-- | .- .-. . | -.-- --- ..- | - --- -.. .- -.-- ..--..
+    
     Args:
-        plain_text (str): regular Latin alphabetic text
+        plain_text (str): A plain message.
 
     Returns:
-        str: The encoded plain_text
-
-    Raises:
-        AttributeError
-            If plain_text is not a string type.
-        TypeError
-            If no plain_text is passed in as an argument.
+        str: An encoded plain_text.
     """
     word = ""
     plain_text = plain_text.upper()
-
-    for letter in plain_text:
-        if(letter in morse_dict):
-            word += morse_dict.get(letter) + " "
+    for symbol in plain_text:
+        if symbol in morse_dict.keys():
+            word += morse_dict[symbol] + " "
         else:
             word += "| "
     return word
 
 
 def values_by_keys(value):
-    """The function returns the key by inputting a value of the key
-       if values not in dictionary, return -1
+    """Returns the dictionary key given the value.
+    
+    Example
+    =======
+    >>> from cryptsenal.morse import *
+    >>> values_by_keys("-...")
+    B
+    
+    Args:
+        value (str): A Morse Code.
 
-       Args:
-           value (str): The portion of the Morse code text
-
-       Returns:
-           str: The key of the dictionary identified by value
-
-       Raises:
-           TypeError
-               If no value is passed in as an argument.
+    Returns:
+        str: A key of the Morse Code Dictionary.
     """
     for keys, values in morse_dict.items():
-        if(value == values):
+        if value == values:
             return keys
     return -1
 
 
 def decode(encoded_text):
-    """The function decodes the encoded message
-
+    """Decodes the encoded message.
+    
+    Example
+    =======
+    >>>from cryptsenal.morse import *
+    >>> decode(".... --- .-- | .- .-. . | -.-- --- ..- | - --- -.. .- -.-- ..--.. ")
+    HOW ARE YOU TODAY? 
+    
     Args:
-        encoded_text (str): The encoded Morse Code text
+        encoded_text (str): A encoded Morse Code text.
 
     Returns:
-        str: The decoded Morse code text
-
-    Raises:
-        AttributeError
-            If encoded_text is not a string type
-        TypeError
-            If no encoded_text is passed in as an argument.
+        str: A decoded Morse code text.
     """
     decoded = ""
     encoded_text = encoded_text.split(" ")
     for code in encoded_text:
-        if " " in code:
-            decoded += " " + values_by_keys(code.strip())
         decoded += str(values_by_keys(code))
-
-    decoded = decoded.replace("-1", " ")
-    return decoded
+    return decoded.replace("-1", " ")
