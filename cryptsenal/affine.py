a2ac670be9502e1be64ef584af5b718b371faa25
AoWangDrexel <aw3338@drexel.edu>
2020-06-17 10:49:53 -0400

    Implement affine cipher

---
+++ cryptsenal/affine.py
@@ -0,0 +1,54 @@
+"""
+description: affine cipher
+author: ao wang
+date: june 17, 2020
+"""
+
+from math import gcd
+from sympy import mod_inverse
+from cryptsenal.cipher import Cipher
+
+
+class Affine(Cipher):
+    """
+
+    """
+
+    def __init__(self, text, key):
+        super().__init__(text)
+        if gcd(key[0], 26) != 1:
+            raise Exception(
+                "a={} must be relatively prime to m=26".format(key[0]))
+        self.key = key
+
+    def __str__(self):
+        a, b = self.key
+        return "text: {}, keys = [a: {}, b: {}]".format(self.text, a, b)
+
+    def encrypt(self):
+        a, b = self.key
+        m = 26
+        arr = self.removePunctuation()
+        arr = [self.intToChar((a*self.charToInt(char)+b) %
+                              m) if char.isalpha() else char for char in arr]
+        return "".join(arr)
+
+    def decrypt(self):
+        a, b = self.key
+        m = 26
+        arr = self.removePunctuation()
+        arr = [self.intToChar((mod_inverse(a, m)*(self.charToInt(char)-b)) %
+                              m) if char.isalpha() else char for char in arr]
+        return "".join(arr)
+
+    def getKey(self):
+        return self.key
+
+    def setKey(self, key):
+        self.key = key
+
+
+if __name__ == "__main__":
+    a = Affine("The quick brown fox jumps over the lazy dog.", (5, 8))
+    print(a)
+    print(a.encrypt())
