d9b8e5c272207de3edea77fddc5031089f79418f
AoWangDrexel <aw3338@drexel.edu>
2020-06-18 12:13:47 -0400

    Update version 0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cryptsenal',
-      version='0.1',
+      version='0.3',
       description='Crytography Aresenal',
       packages=['cryptsenal'],
       zip_safe=False)
