492f24b14c52ad1d766478a62f324d125731988d
AoWangDrexel <aw3338@drexel.edu>
2020-06-18 23:16:48 -0400

    Implement the simple substitution cipher

--- cryptsenal/simple_substitution.py
+++ cryptsenal/simple_substitution.py
@@ -1,12 +1,37 @@
+"""
+description: simple substitution cipher
+author: ao wang
+date: june 18, 2020
+"""
+
 from cryptsenal.cipher import Cipher
+import string
 
 
 class SimpleSubstitution(Cipher):
+    """The Simple Substitution class
+
+    :param text: the plain/cipher text
+    :type text: str
+    :param key: the cipher key
+    :type key: str
+    """
+
     def __init__(self, text, key):
-        super().__init__(text, key)
+        super().__init__(text, key.upper())
 
     def encrypt(self):
-        return super().encrypt()
+        arr = self.removePunctuation()
+        arr = [self.key[self.charToInt(char)] for char in arr]
+        return "".join(arr)
 
     def decrypt(self):
-        return super().decrypt()
+        arr = self.removePunctuation()
+        arr = [string.ascii_uppercase[self.key.find(char)] for char in arr]
+        return "".join(arr)
+
+
+if __name__ == "__main__":
+    msg = "GIUIFGCEIIPRCTPNNDUCEIQPRCNI"
+    key = "phqgiumeaylnofdxjkrcvstzwb"
+    print(SimpleSubstitution(msg, key).decrypt())
