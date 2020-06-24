ee6084722b5c53638967c81ceda31c4a14809e13
AoWangDrexel <aw3338@drexel.edu>
2020-06-24 12:01:20 -0400

    Add RSA generate key class

---
+++ cryptsenal/generate_rsa_key.py
@@ -0,0 +1,43 @@
+from math import gcd
+from sympy import isprime, mod_inverse
+from numpy import lcm
+
+
+class RSAKey():
+    def __init__(self, p, q):
+        self.p = p
+        self.q = q
+        if isprime(p) and isprime(q):
+            self.N = p * q
+            self.totient = lcm(p-1, q-1)
+        else:
+            raise Exception("p or q are not prime numbers")
+
+    def getE(self):
+        return [i for i in range(2, self.totient) if gcd(i, self.totient) == 1]
+
+    def getD(self, E):
+        return mod_inverse(E, self.totient)
+
+    def getP(self):
+        return self.p
+
+    def setP(self, p):
+        if isprime(p):
+            self.p = p
+        else:
+            raise Exception("p is not a prime number")
+
+    def getQ(self):
+        return self.q
+
+    def setQ(self, q):
+        if isprime(q):
+            self.q = q
+        else:
+            raise Exception("q is not a prime number")
+
+
+if __name__ == "__main__":
+    rsa = RSAKey(61, 53)
+    print(rsa.N, rsa.totient, rsa.getE(), rsa.getD(17))
